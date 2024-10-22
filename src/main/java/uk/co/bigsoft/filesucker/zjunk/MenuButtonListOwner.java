8757366eff13ff5f9f87d7ccd7adc9c03414268b
David Newcomb <david.newcomb@bigsoft.co.uk>
2024-10-22 20:48:43 +0100

    Almost there, remove junk

--- src/main/java/uk/co/bigsoft/filesucker/zjunk/MenuButtonListOwner.java
+++
@@ -1,9 +0,0 @@
-package uk.co.bigsoft.filesucker.zjunk;
-
-import uk.co.bigsoft.filesucker.view.HistoryDropDown;
-
-public interface MenuButtonListOwner {
-	public HistoryDropDown getList();
-
-	public void setList(HistoryDropDown list);
-}
--- src/main/java/uk/co/bigsoft/filesucker/zjunk/MenuButtonListener.java
+++
@@ -1,5 +0,0 @@
-package uk.co.bigsoft.filesucker.zjunk;
-
-public interface MenuButtonListener {
-	void changed(String newText);
-}
